cdefa87ed3c6e4e9bac2c486ce7d10cfc6e21c7b
unknown <christinesoto21@gmail.com>
2020-02-11 14:09:57 -0600

    Classes and inheritance added

---
+++ RockPaper/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaper
+{
+    public class Human : Player
+    {
+        //Member Variables
+
+        //Constructor
+        public Human(string name)
+        {
+            this.name = name;
+            int score = 0;
+        }
+        
+        //Member Methods
+
+    }
+}
